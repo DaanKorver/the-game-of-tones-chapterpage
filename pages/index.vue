fe2d55b38af67dfcedd31e7c6eab4bc2d3e7c48b
justin <lungjyt@hva.nl>
2021-12-10 09:47:24 +0100

    basic styling made

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <Tutorial/>
+  <div>
+    <section>
+      <h1>The Game of Tones</h1>
+      <p>Creative Music Making</p>
+    </section>
+    <NuxtLink to="Login">SKIP</NuxtLink>
+  </div>
 </template>
-
 <script>
-export default {}
+export default {
+
+}
 </script>
+<style scoped>
+
+</style>
