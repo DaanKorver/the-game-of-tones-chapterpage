91d2a74bdfa8960f46ef97eba059c30d1e1e8f5f
DaanKorver <DaanKorver@users.noreply.github.com>
2021-12-22 20:44:36 +0000

    deploy: d0c4df5d581355402f9c5cd5ab9fb1fab1dd2968

---
+++ _nuxt/static/1640205875/volume/1/chapter/1/intro/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/volume/1/chapter/1/intro", {data:[{title:"Introduction Chapter 1",body:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti facilis eum nobis possimus sint, odio ratione odit alias quos nostrum ipsam iusto explicabo beatae cupiditate dolorum. Consequatur commodi libero doloribus."}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1640205875/volume/1/menu/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/volume/1/menu", {data:[{}],fetch:{},mutations:void 0});
