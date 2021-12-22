c65a9ded3a1a9eb0bcdde98e7144c8a359b19fc0
Justin Lung <justin.lung@hva.nl>
2021-12-22 13:04:58 +0100

    fixed accessibility and SEO issues

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,15 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'the-game-of-tones-chapterpage',
+    title: 'Game of Tones',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: 'Webapp for learning music' },
+      { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
