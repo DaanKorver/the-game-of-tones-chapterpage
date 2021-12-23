ce4e7170ca369ff9f1a916fae150618f07ee94d5
DaanKorver <DaanKorver@users.noreply.github.com>
2021-12-23 08:40:42 +0000

    deploy: 57b5ead89bf26c3c5622e8be6f55ac007b8bc402

---
+++ _nuxt/static/1640248841/volume/1/chapter/1/lesson/1/subject/1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1640248841",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvolume\u002F1\u002Fchapter\u002F1\u002Flesson\u002F1\u002Fsubject\u002F1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
